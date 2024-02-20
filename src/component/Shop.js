0b86727cec30d13a0bfc5e5248a389124ae9bc54
Hariom Singh <22169420+c9hariom@users.noreply.github.com>
2024-02-20 21:00:58 +0530

    redux testa app nav and shop component

---
+++ src/component/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h2>Buy Adidas Shoes 50Rs</h2>
+      <button className='btn btn-primary m-2'>-</button>
+        Add this Item to Cart
+      <button className='btn btn-primary m-2'>+</button>
+    </div>
+  )
+}
+
+export default Shop
